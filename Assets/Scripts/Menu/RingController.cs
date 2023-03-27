42644e77768982ba5d627b90074e62ff52d23a83
unknown <xiaoanpan@intranet.123u.com>
2023-03-27 19:52:09 +0800

    新增 Beat的圆环 2

--- Assets/Scripts/Menu/RingController.cs
+++ Assets/Scripts/Menu/RingController.cs
@@ -8,6 +8,7 @@ public class RingController : MonoBehaviour
     float x,st,en;
     float speed;
     int times,nowtime;
+    int stop;
     public GameObject ir,or;
     RectTransform irmt,ormt;
     // Start is called before the first frame update
@@ -15,6 +16,7 @@ public class RingController : MonoBehaviour
         x=ToolMgr.Instance.GetRingTime();
         st=400;
         en=200;
+        stop=0;
         irmt = ir.GetComponent<Image>().GetComponent<RectTransform>();
         ormt = or.GetComponent<Image>().GetComponent<RectTransform>();
         irmt.sizeDelta = new Vector2(en,en);
@@ -32,20 +34,29 @@ public class RingController : MonoBehaviour
     }
     private void doupdate(string event_name, object udata)
     {
+        if(stop==1)
+        {
+            EventMgr.Instance.RemoveListener("ROperation",doupdate);
+            UIRoot.Instance.uninit("Ring",0);
+        }
         if(nowtime<times)
         {
             st-=speed;
             nowtime++;
-
         }
         else
         {
-            UIRoot.Instance.uninit("Ring",0);
             EventMgr.Instance.RemoveListener("ROperation",doupdate);
+            UIRoot.Instance.uninit("Ring",0);
         }
     }
 
     private void Update() {
         ormt.sizeDelta = new Vector2(st,st);
+    }
+
+    public void stopcount()
+    {
+        stop=1;
     }
 }
